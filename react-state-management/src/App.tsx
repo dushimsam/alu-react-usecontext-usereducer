08bce6031f0ced8013699d001eac574f42da9585
dushsam <dushsam100@gmail.com>
2026-09-26 19:58:17 +0200

    feat: integrate Navbar and TaskManager in App.tsx

---
+++ react-state-management/src/App.tsx
@@ -0,0 +1,16 @@
+
+import { ThemeProvider } from './context/ThemeContext';
+import Navbar from './components/Navbar';
+import TaskManager from './components/TaskManager';
+import './App.css';
+
+function App() {
+  return (
+    <ThemeProvider>
+      <Navbar />
+      <TaskManager />
+    </ThemeProvider>
+  );
+}
+
+export default App;
